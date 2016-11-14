853fd29629d676323b8577da8bdf9aa0084a8bc3
sanders76 <s.teunissen@gmail.com>
2016-11-14 16:47:36 +0100

    adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='figureAltCaption',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='steun',
+    author_email='',
+    description=''
+)
